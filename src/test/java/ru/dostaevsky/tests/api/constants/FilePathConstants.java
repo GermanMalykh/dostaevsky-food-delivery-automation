336a7898401125d0f8f2ef58d16f7ab3d1574447
germanmalykh <malykh.gn@esphere.ru>
2023-06-27 00:27:06 +0300

    Add json converter for UpdateItemCountInTheBasketTests

---
+++ src/test/java/ru/dostaevsky/tests/api/constants/FilePathConstants.java
@@ -0,0 +1,9 @@
+package ru.dostaevsky.tests.api.constants;
+
+import java.io.File;
+
+public class FilePathConstants {
+    public static final String TEST_RESOURCES = "src" + File.separator + "test" + File.separator + "resources" + File.separator;
+    public static final String JSON_PATH = TEST_RESOURCES + "jsons" + File.separator;
+    public static final String UPDATE_ITEM_COUNT_IN_THE_BASKET_JSON = JSON_PATH + "updateItemCountInTheBasket.json";
+}
