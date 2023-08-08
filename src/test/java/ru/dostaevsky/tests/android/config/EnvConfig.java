2a614f80bbd20e72735267e7cd7b9b3d815c5ae2
germanmalykh <malykh.gn@esphere.ru>
2023-08-08 23:43:04 +0300

    fix config

--- src/test/java/ru/dostaevsky/tests/android/config/EnvConfig.java
+++ src/test/java/ru/dostaevsky/tests/android/config/EnvConfig.java
@@ -5,14 +5,19 @@ import org.aeonbits.owner.Config;
 @Config.Sources("classpath:configs/mobile/${env}.properties")
 public interface EnvConfig extends Config {
 
+    @Key("app")
     String app();
 
+    @Key("device")
     String device();
 
+    @Key("os_version")
     String os_version();
 
+    @Key("appPackage")
     String appPackage();
 
+    @Key("appActivity")
     String appActivity();
 
     @Key("browserstack.user")
@@ -21,6 +26,7 @@ public interface EnvConfig extends Config {
     @Key("browserstack.key")
     String key();
 
+    @Key("appiumUrl")
     String appiumUrl();
 
 }
--- src/test/java/ru/dostaevsky/tests/web/config/EnvConfig.java
+++ src/test/java/ru/dostaevsky/tests/web/config/EnvConfig.java
@@ -5,12 +5,16 @@ import org.aeonbits.owner.Config;
 @Config.Sources("classpath:configs/web/${env}.properties")
 public interface EnvConfig extends Config {
 
+    @Key("browser_name")
     String browser_name();
 
+    @Key("browser_version")
     String browser_version();
 
+    @Key("browser_size")
     String browser_size();
 
+    @Key("selenoid_url")
     String selenoid_url();
 
 }
