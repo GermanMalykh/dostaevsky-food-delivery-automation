63ac093492e152e7b59f1d9879faf60ab0213334
German Malykh <108834841+GermanMalykh@users.noreply.github.com>
2025-08-30 17:35:59 +0300

    Update FavoriteInfoTests.java

--- src/test/java/ru/dostaevsky/tests/android/tests/FavoriteInfoTests.java
+++ src/test/java/ru/dostaevsky/tests/android/tests/FavoriteInfoTests.java
@@ -1,7 +1,7 @@
 package ru.dostaevsky.tests.android.tests;
 
+import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -14,14 +14,10 @@ import ru.dostaevsky.tests.android.pages.components.NavigationComponents;
 import static io.qameta.allure.SeverityLevel.NORMAL;
 import static ru.dostaevsky.data.MenuItemsData.ADDITIONAL_INFO;
 import static ru.dostaevsky.data.MenuItemsData.FAVORITE;
-import static ru.dostaevsky.enums.Category.ONIGIRI;
+import static ru.dostaevsky.enums.Category.DESSERTS;
 import static ru.dostaevsky.enums.CityName.SPB;
-import static ru.dostaevsky.enums.Onigiri.SNOW_CRAB_ONIGIRI;
+import static ru.dostaevsky.enums.Desserts.CARAMEL_CHEESECAKE;
 
-@Disabled(
-        "Тест выключен из-за проблем со скроллом через эмулятор на BROWSERSTACK. " +
-        "Включить для локального запуска"
-)
 @Tag("android")
 @DisplayName("Android Tests")
 public class FavoriteInfoTests extends PreRunConfig {
@@ -30,19 +26,23 @@ public class FavoriteInfoTests extends PreRunConfig {
     ItemComponents item = new ItemComponents();
     FavoritePage favorite = new FavoritePage();
 
+    @Tag("only-local")
     @Severity(NORMAL)
+    @Description(
+            "Тест выключен из-за проблем со скроллом через эмулятор на BROWSERSTACK. " +
+                    "Только для локального запуска"
+    )
     @DisplayName("[Android] Добавление товара в избранное и проверка отображения товара в списке избранных")
     @Test
     void addItemToFavoriteListAndCheckIt() {
-        main.selectByText(SPB.getDisplayName())
-                .closingTechInfo();
-        navigation.scrollToElement(ONIGIRI.getValue())
-                .scrollToElement(SNOW_CRAB_ONIGIRI.getValue());
+        main.selectByText(SPB.getDisplayName());
+        navigation.scrollToElement(DESSERTS.getValue())
+                .scrollToElement(CARAMEL_CHEESECAKE.getValue());
         item.addItemToFavorite();
         navigation.backNavigation();
         main.selectByText(ADDITIONAL_INFO);
         main.selectByText(FAVORITE);
         favorite.checkFavoriteTitle();
-        item.checkProductName(SNOW_CRAB_ONIGIRI.getValue());
+        item.checkProductName(CARAMEL_CHEESECAKE.getValue());
     }
 }
