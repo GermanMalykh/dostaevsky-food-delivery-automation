21c5940592004ae09bc944ff3179d114b99d7965
German Malykh <108834841+GermanMalykh@users.noreply.github.com>
2025-08-30 17:34:47 +0300

    Update ItemComponents.java

--- src/test/java/ru/dostaevsky/tests/android/pages/components/ItemComponents.java
+++ src/test/java/ru/dostaevsky/tests/android/pages/components/ItemComponents.java
@@ -6,6 +6,7 @@ import io.qameta.allure.Step;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 
@@ -21,6 +22,7 @@ public class ItemComponents {
 
     @Step("Добавляем позицию в корзину")
     public ItemComponents addProductToCart() {
+        sleep(1_500);
         addProductButton.click();
         return this;
     }
